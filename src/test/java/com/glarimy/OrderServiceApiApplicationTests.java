b63da569580a1438d45896a05d22492da4653dcc
Abhijeet Dey <47703148+Abhi-Dey@users.noreply.github.com>
2023-06-30 00:56:54 +0530

    Update OrderServiceApiApplicationTests.java

--- src/test/java/com/glarimy/OrderServiceApiApplicationTests.java
+++ src/test/java/com/glarimy/OrderServiceApiApplicationTests.java
@@ -3,11 +3,17 @@ package com.glarimy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class OrderServiceApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // Add your test logic here
+        // For example, assert that a certain condition is true
+        assertTrue(1 + 1 == 2);
+    }
 
 }
+
